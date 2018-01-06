cb733fafaf71be8c35ed032e21458cfce52adfe8
Andrew <andrew.d.boyd@gmail.com>
2018-01-06 17:17:46 +0000

    latest code

--- cookbooks/apache/recipes/motd.rb
+++ cookbooks/apache/recipes/motd.rb
@@ -1,4 +1,12 @@
 hostname = node['hostname']
+#package 'fortune' do
+#  action :install
+#end
+
+package 'cowsay' do
+  action :install
+end
+
 file '/etc/motd' do
-  content "Hostname is this: #{hostname}"
+  content "Hostname is this: #{hostname} {fortune | cowsay}" 
 end
